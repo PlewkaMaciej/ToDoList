c85c7deb50869bef1c6cc50ebf72c4c2f2adeb9e
Maciej <plewka-maciej@wp.pl>
2022-04-28 20:10:59 +0200

    user can  add new things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import "./app.css"
 import 'bootstrap/dist/css/bootstrap.css';
 import Button from "react-bootstrap/Button";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 function App() {
-  const [toDoThings,setDoThings]=useState(["jablko"])
+  const inputRef=useRef()
+  const [toDoThings, setDoThings] = useState(["jablko"])
+  const [inputValue, setInputValue] = useState()
+  const [clearInput, setClearInput] = useState(0)
+  const getInputValue = (e) => {
+    setInputValue(e.target.value)
+  }
+  const addNewThingToDo = () => {
+    setDoThings(prevInputValue => [...prevInputValue,inputValue])
+    console.log(toDoThings)
+    setInputValue("")
+    inputRef.current.value=""
+  }
+
   return (
     <>
-    <div className="mainContainer">
-      <h1 className="title-heading">ToDoList</h1>
-      <div className="paragraph-and-input-container">
-        <p className="add-paragraph">Add thing to your list</p>
-        <input className="new-thingToDoInput" type="text" />
-        <Button variant="dark">
+      <div className="mainContainer">
+        <h1 className="title-heading">ToDoList</h1>
+        <div className="paragraph-and-input-container">
+          <p className="add-paragraph">Add thing to your list</p>
+          <input ref={inputRef} className="new-thingToDoInput" onChange={getInputValue} type="text" />
+          <Button onClick={addNewThingToDo} variant="dark">
             add
-        </Button>
+          </Button>
+        </div>
+        <div className="container-for-adding-elements">
+          {toDoThings.map((value,id) => {
+            return <div className="value-and-button-for-delete" key={id}> <p>{value}</p> <Button className="DeleteButton" variant="light">X </Button></div>
+          })}
+        </div>
       </div>
-<div className="container-for-adding-elements">
-{toDoThings.filter((value)=>{
-  return <p>value</p>
-})}
-</div>
-    </div>
     </>
   );
 }
