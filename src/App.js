e72b5778e884e8e6717caee3362a081e210fe2c1
Maciej <plewka-maciej@wp.pl>
2022-04-29 00:20:10 +0200

    user can delete tasks

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Button from "react-bootstrap/Button";
 import React, { useState, useRef } from 'react';
 function App() {
-  const inputRef=useRef()
+  const inputRef = useRef()
   const [toDoThings, setDoThings] = useState(["jablko"])
   const [inputValue, setInputValue] = useState()
-  const [clearInput, setClearInput] = useState(0)
+  const DeleteThingsFromToDoList = (e) => {
+    
+    let newList = toDoThings
+    newList.splice(e.target.id,1);
+    setDoThings([...newList])
+  }
   const getInputValue = (e) => {
     setInputValue(e.target.value)
   }
   const addNewThingToDo = () => {
-    setDoThings(prevInputValue => [...prevInputValue,inputValue])
-    console.log(toDoThings)
+    setDoThings(prevInputValue => [...prevInputValue, inputValue])
     setInputValue("")
-    inputRef.current.value=""
+    inputRef.current.value = ""
   }
 
   return (
@@ -29,8 +33,8 @@ function App() {
           </Button>
         </div>
         <div className="container-for-adding-elements">
-          {toDoThings.map((value,id) => {
-            return <div className="value-and-button-for-delete" key={id}> <p>{value}</p> <Button className="DeleteButton" variant="light">X </Button></div>
+          {toDoThings.map((value, id) => {
+            return <div className="value-and-button-for-delete" key={id}> <p>{value}</p> <Button id={id} onClick={DeleteThingsFromToDoList} className="DeleteButton" variant="light">X </Button></div>
           })}
         </div>
       </div>
